0a51ebf9c52fe4f1b48f80aa5587b96f727984ab
hngnch8bzk@yahoo.co.jp <hngnch8bzk@yahoo.co.jp>
2023-06-18 21:34:10 +0900

    [add]Modelをシングルトンで保持するWorkspaceクラスを作成
    ViewModelに注入すると、xamlに書くDataContextが大変なことになるため。

---
+++ Models/Workspace.cs
@@ -0,0 +1,44 @@
+﻿namespace GraDeMarCoWPF.Models
+{
+    public class Workspace
+    {
+        public static Workspace Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        private static Workspace _instance = new Workspace();
+
+        private Workspace()
+        {
+            ModelA = new ModelA();
+            ModelB = new ModelB(ModelA);
+        }
+
+        public ModelA ModelA { get; private set; }
+        public ModelB ModelB { get; private set; }
+    }
+
+    public class ModelA
+    {
+        public string a { get; set; }
+
+        public ModelA()
+        {
+
+        }
+    }
+
+    public class ModelB
+    {
+        private ModelA a;
+
+        public ModelB(ModelA a)
+        {
+            this.a = a;
+        }
+    }
+}
