663f1a9ce52b1c3d9119cd307132776b5066dc49
hngnch8bzk@yahoo.co.jp <hngnch8bzk@yahoo.co.jp>
2023-06-20 21:49:17 +0900

    [add]DelegateCommandを分離

---
+++ Commands/DelegateCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace GraDeMarCoWPF.Commands
+{
+    public class DelegateCommand : ICommand
+    {
+        private Action<object> command;        // コマンド本体
+        private Func<object, bool> canExecute;  // 実行可否
+
+        public DelegateCommand(Action<object> command, Func<object, bool> canExecute = null)
+        {
+            if (command == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            this.command = command;
+            this.canExecute = canExecute;
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            command(parameter);
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            if (canExecute != null)
+            {
+                return canExecute(parameter);
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
--- ViewModels/ViewModelBase.cs
+++ ViewModels/ViewModelBase.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using GraDeMarCoWPF.Commands;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace GraDeMarCoWPF.ViewModels
@@ -66,55 +64,10 @@ namespace GraDeMarCoWPF.ViewModels
             }
         }
 
-        // ICommand実装用のヘルパークラス
-        private class _DelegateCommand : ICommand
-        {
-            private Action<object> _Command;        // コマンド本体
-            private Func<object, bool> _CanExecute;  // 実行可否
-
-            // コンストラクタ
-            public _DelegateCommand(Action<object> command, Func<object, bool> canExecute = null)
-            {
-                if (command == null)
-                {
-                    throw new ArgumentNullException();
-                }
-
-                _Command = command;
-                _CanExecute = canExecute;
-            }
-
-            // ICommand.Executeの実装
-            void ICommand.Execute(object parameter)
-            {
-                _Command(parameter);
-            }
-
-            // ICommand.Executeの実装
-            bool ICommand.CanExecute(object parameter)
-            {
-                if (_CanExecute != null)
-                {
-                    return _CanExecute(parameter);
-                }
-                else
-                {
-                    return true;
-                }
-            }
-
-            // ICommand.CanExecuteChanged の実装
-            event EventHandler ICommand.CanExecuteChanged
-            {
-                add { CommandManager.RequerySuggested += value; }
-                remove { CommandManager.RequerySuggested -= value; }
-            }
-        }
-
         // コマンドの生成
         protected ICommand CreateCommand(Action<object> command, Func<object, bool> canExecute = null)
         {
-            return new _DelegateCommand(command, canExecute);
+            return new DelegateCommand(command, canExecute);
         }
     }
 }
