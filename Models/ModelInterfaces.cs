527fcfbedbb3744e913d93fc0e1f3d4b18d228f6
hngnch8bzk@yahoo.co.jp <hngnch8bzk@yahoo.co.jp>
2023-06-24 07:34:16 +0900

    [add]Model用のインターフェイスを追加

---
+++ Models/ModelInterfaces.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Media;
+
+namespace GraDeMarCoWPF.Models
+{
+    public interface IToggleFunction
+    {
+        void Start();
+        void Stop();
+    }
+
+    public interface IDrawingOnRender
+    {
+        void DrawOnRender(DrawingContext drawingContext);
+    }
+
+    public interface IDrawingOnImageSource
+    {
+        void DrawOnImageSource(ImageSource imageSource);
+    }
+}
