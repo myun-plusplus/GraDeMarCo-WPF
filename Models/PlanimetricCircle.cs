b8130bf8db0331dc532e6fa8f2bb84c5bbc50455
hngnch8bzk@yahoo.co.jp <hngnch8bzk@yahoo.co.jp>
2023-07-02 08:08:47 +0900

    [add]データ保存用のPlanimetricCircleモデルを作成

---
+++ Models/PlanimetricCircle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GraDeMarCoWPF.Models
+{
+    [Serializable]
+    public class PlanimetricCircle : BindableBase
+    {
+        public int LowerX
+        {
+            get { return _lowerX; }
+            set
+            {
+                _lowerX = value;
+                NotifyPropertyChanged(GetName.Of(() => LowerX));
+            }
+        }
+        public int LowerY
+        {
+            get { return _lowerY; }
+            set
+            {
+                _lowerY = value;
+                NotifyPropertyChanged(GetName.Of(() => LowerY));
+            }
+        }
+
+        public int Diameter
+        {
+            get { return _diameter; }
+            set
+            {
+                _diameter = value;
+                NotifyPropertyChanged(GetName.Of(() => Diameter));
+            }
+        }
+
+        private int _lowerX;
+        private int _lowerY;
+        private int _diameter;
+    }
+}
