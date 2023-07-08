503aad6c2e0e0f3e369bfad88c943c3dc3a62db5
hngnch8bzk@yahoo.co.jp <hngnch8bzk@yahoo.co.jp>
2023-07-08 21:49:09 +0900

    [add]SaveFileDialogServiceを作成

---
+++ Services/SaveFileDialogService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+
+namespace GraDeMarCoWPF.Services
+{
+    internal class SaveFileDialogService : ISaveFileDialogService
+    {
+        private SaveFileDialog saveFileDialog;
+
+        public SaveFileDialogService(string title, string filter)
+        {
+            saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = title;
+            saveFileDialog.Filter = filter;
+            saveFileDialog.FilterIndex = 0;
+            saveFileDialog.RestoreDirectory = true;
+        }
+
+        public string Filename { get; set; }
+
+        public bool? ShowDialog()
+        {
+            return saveFileDialog.ShowDialog();
+        }
+    }
+}
