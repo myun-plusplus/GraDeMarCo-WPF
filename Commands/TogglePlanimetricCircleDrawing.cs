e69cbee58dac8179bdcb39964d7fe17a88b39451
hngnch8bzk@yahoo.co.jp <hngnch8bzk@yahoo.co.jp>
2023-07-08 16:57:25 +0900

    [fix]CurrentStateを変更するようにした

--- Commands/TogglePlanimetricCircleDrawing.cs
+++ Commands/TogglePlanimetricCircleDrawing.cs
@@ -1,5 +1,6 @@
 ﻿using GraDeMarCoWPF.Models;
 using System;
+using System.ComponentModel;
 using System.Windows.Input;
 
 namespace GraDeMarCoWPF.Commands
@@ -17,6 +18,8 @@ namespace GraDeMarCoWPF.Commands
         {
             this.appData = appData;
             this.planimetricCircleDrawing = planimetricCircleDrawing;
+
+            appData.PropertyChanged += this.appData_PropertyChanged;
         }
 
         public bool CanExecute(object parameter)
@@ -29,11 +32,21 @@ namespace GraDeMarCoWPF.Commands
         {
             if ((parameter as bool?) ?? false)
             {
+                appData.CurrentState = AppState.PlanimetricCircleDrawing;
                 planimetricCircleDrawing.StartFunction();
             }
             else
             {
+                appData.CurrentState = AppState.ImageOpened;
                 planimetricCircleDrawing.StopFunction();
+            }
+        }
+
+        private void appData_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, new EventArgs());
             }
         }
     }
