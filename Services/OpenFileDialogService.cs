81e0ce60348826a7efb00f9f33f3295e031f0068
hngnch8bzk@yahoo.co.jp <hngnch8bzk@yahoo.co.jp>
2023-07-08 21:23:37 +0900

    [add]OpenFileDialogServiceを作成

---
+++ Services/OpenFileDialogService.cs
@@ -0,0 +1,26 @@
+﻿using GraDeMarCoWPF.Models;
+using Microsoft.Win32;
+
+namespace GraDeMarCoWPF.Services
+{
+    public class OpenFileDialogService : IOpenFileDialogService
+    {
+        private OpenFileDialog openFileDialog;
+
+        public OpenFileDialogService(string title, string filter)
+        {
+            openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = title;
+            openFileDialog.Filter = filter;
+            openFileDialog.FilterIndex = 0;
+            openFileDialog.RestoreDirectory = true;
+        }
+
+        public string Filename { get; set; }
+
+        public bool? ShowDialog()
+        {
+            return openFileDialog.ShowDialog();
+        }
+    }
+}
