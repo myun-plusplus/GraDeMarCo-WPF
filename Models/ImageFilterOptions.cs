4db18eec851236a19c17e43313bff3f807e1eff6
hngnch8bzk@yahoo.co.jp <hngnch8bzk@yahoo.co.jp>
2023-07-13 21:46:35 +0900

    [add]ImageFilteringのオプションを保持するクラスを作成

---
+++ Models/ImageFilterOptions.cs
@@ -0,0 +1,45 @@
+﻿using System.Xml.Linq;
+
+namespace GraDeMarCoWPF.Models
+{
+    public enum BlurOption
+    {
+        None,
+        Gaussian,
+        Gaussian3Times,
+        Gaussian6Times
+    }
+
+    public enum EdgeDetectOption
+    {
+        None,
+        Sobel,
+        Laplacian
+    }
+
+    public class ImageFilterOptions : BindableBase
+    {
+        public BlurOption BlurOption
+        {
+            get { return _blurOption; }
+            set
+            {
+                _blurOption = value;
+                NotifyPropertyChanged(GetName.Of(() => BlurOption));
+            }
+        }
+
+        public EdgeDetectOption EdgeDetectOption
+        {
+            get { return _edgeDetectOption; }
+            set
+            {
+                _edgeDetectOption = value;
+                NotifyPropertyChanged(GetName.Of(() => EdgeDetectOption));
+            }
+        }
+
+        private BlurOption _blurOption;
+        private EdgeDetectOption _edgeDetectOption;
+    }
+}
