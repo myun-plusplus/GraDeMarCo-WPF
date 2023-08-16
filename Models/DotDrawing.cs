7112c53776f26f354034d15eb858b4af13c64ebb
hngnch8bzk@yahoo.co.jp <hngnch8bzk@yahoo.co.jp>
2023-08-16 21:56:46 +0900

    [add]DotDrawingのクリック、ボタン周りを実装した

--- Models/DotDrawing.cs
+++ Models/DotDrawing.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Linq;
+using System.Windows;
 using System.Windows.Media;
 
 namespace GraDeMarCoWPF.Models
@@ -45,12 +47,36 @@ namespace GraDeMarCoWPF.Models
 
         public void LeftClick(Point location)
         {
+            Dot dot = new Dot
+            {
+                Location = location,
+                Color = drawnDotDrawingTool.Color,
+                Size = drawnDotDrawingTool.Size
+            };
 
+            drawnDotData.Dots.Add(dot);
+
+            drawnDotData.DoneList.Add(Tuple.Create(dot.Clone(), false));
+            drawnDotData.UndoList.Clear();
         }
 
         public void RightClick(Point location)
         {
+            var di_min = drawnDotData.Dots
+                .Select(dot => getDistance(dot.Location, location))
+                .Zip(Enumerable.Range(0, drawnDotData.Dots.Count), (distance, index) => Tuple.Create(distance, index))
+                .Min();
 
+            // 適当
+            if (di_min.Item1 < 16.0)
+            {
+                Dot dot = drawnDotData.Dots[di_min.Item2];
+
+                drawnDotData.Dots.RemoveAt(di_min.Item2);
+
+                drawnDotData.DoneList.Add(Tuple.Create(dot, true));
+                drawnDotData.UndoList.Clear();
+            }
         }
 
         public void MouseMove(Point location)
@@ -63,17 +89,56 @@ namespace GraDeMarCoWPF.Models
 
         public void Undo()
         {
+            if (drawnDotData.DoneList.Count == 0)
+            {
+                return;
+            }
 
+            var t = drawnDotData.DoneList.Last();
+            if (!t.Item2)
+            {
+                drawnDotData.Dots.RemoveAt(drawnDotData.Dots.Count - 1);
+            }
+            else
+            {
+                drawnDotData.Dots.Add(t.Item1.Clone());
+            }
+
+            drawnDotData.UndoList.Add(t);
+            drawnDotData.DoneList.RemoveAt(drawnDotData.DoneList.Count - 1);
         }
 
         public void Redo()
         {
+            if (drawnDotData.UndoList.Count == 0)
+            {
+                return;
+            }
 
+            var t = drawnDotData.UndoList.Last();
+            if (!t.Item2)
+            {
+                drawnDotData.Dots.Add(t.Item1.Clone());
+            }
+            else
+            {
+                drawnDotData.Dots.RemoveAt(drawnDotData.Dots.Count - 1);
+            }
+
+            drawnDotData.DoneList.Add(t);
+            drawnDotData.UndoList.RemoveAt(drawnDotData.UndoList.Count - 1);
         }
 
         public void Clear()
         {
+            drawnDotData.Dots.Reverse();
+            drawnDotData.DoneList = drawnDotData.Dots
+                .Select(dot => Tuple.Create(dot, true))
+                .ToList();
+            drawnDotData.UndoList.Clear();
 
+            drawnDotData.Dots.Clear();
+            detectedDotData.Dots.Clear();
         }
 
         public void DrawOnDynamicRendering(DrawingContext drawingContext)
@@ -129,6 +194,11 @@ namespace GraDeMarCoWPF.Models
                 Rect rect = new Rect(location.X, location.Y, dot.Size, dot.Size);
                 drawingContext.DrawRectangle(brush, null, rect);
             }
+        }
+
+        private static double getDistance(Point p1, Point p2)
+        {
+            return Math.Sqrt((p2.X - p1.X) * (p2.X - p1.X) + (p2.Y - p1.Y) * (p2.Y - p1.Y));
         }
 
         private Point _mouseLocation;
